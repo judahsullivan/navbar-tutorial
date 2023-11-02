2c92f71f57daab78897baa11aad438ce2c59c3de
judahsullivan <judahsullivan.dev@gmail.com>
2023-11-02 18:38:06 -0500

    added information to the head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,12 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <div className="bg-gray-200">
+      <head>
+        <title>Animated Navbar</title>
+        <description>
+          Building a animated navbar with tailwindcss, framer-motion and nextjs
+        </description>
+      </head>
       <Navbar />
       <Component {...pageProps} />
     </div>
